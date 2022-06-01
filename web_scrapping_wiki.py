687d375de0eb01bfc8e881bcae1ec4017b1c5536
Thomas <vaudescal.t@gmail.com>
2022-06-01 17:54:37 -0400

    new changes in web_scrapping_wiki.py

--- web_scrapping_wiki.py
+++ web_scrapping_wiki.py
@@ -11,8 +11,21 @@ page = requests.get(url)
 
 # On change le parseur pour qu'il soit plus compatible avec le format Python
 # On obtient l'information de la page
+# Ne pas oublier de pip install le parseur avant de lancer le script
 
-soup = BeautifulSoup(page.text, 'lxml')
+soup = BeautifulSoup(page.text, "lxml")
+
+# On obtient l'information depuis le tag HTML <table>
+
+table1 = soup.find("table", {"class": "wikitable sortable"}, id="constituents")
+
+# Le but est maintenant d'obtenir tous les titres de chaque colonne grâce au tag <th>
+
+headers = []
+for i in table1.find_all("th"):
+    title = i.text
+    headers.append(title)
+
 
 
 
