3365083b0a9ea0a5a8dd57910d20922a8e3ca17d
Herosbrine <gregoiretheboss@gmail.com>
2022-06-07 01:53:43 +0200

    [ADD] | try and expect

--- web_scrapping_wiki.py
+++ web_scrapping_wiki.py
@@ -7,37 +7,45 @@ import pandas as pd
 
 
 def GetTable():
-    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    headers = []
+    try:
+        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        headers = []
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "lxml")
-    table = soup.find(
-        "table", {"class": "wikitable sortable"}, id="constituents")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "lxml")
+        table = soup.find(
+            "table", {"class": "wikitable sortable"}, id="constituents")
 
-    for i in table.find_all("th"):
-        title = i.text
-        headers.append(title)
+        for i in table.find_all("th"):
+            title = i.text
+            headers.append(title)
 
-    headers_clean = [header.replace("\n", "") for header in headers]
-    return (headers_clean, table)
+        headers_clean = [header.replace("\n", "") for header in headers]
+        return (headers_clean, table)
+    except:
+        print("Error getting table")
+        exit(84)
 
 
 def SaveTab(headers_clean, tab):
-    df = pd.DataFrame(columns=headers_clean)
+    try:
+        df = pd.DataFrame(columns=headers_clean)
 
-    for j in tab.find_all("tr")[1:]:
-        row_data = j.find_all("td")
-        row = [i.text for i in row_data]
-        length = len(df)
-        df.loc[length] = row
+        for j in tab.find_all("tr")[1:]:
+            row_data = j.find_all("td")
+            row = [i.text for i in row_data]
+            length = len(df)
+            df.loc[length] = row
 
-    for col in df.columns:
-        df[col] = df[col].str.replace("\n", "")
+        for col in df.columns:
+            df[col] = df[col].str.replace("\n", "")
 
-    df["Founded"] = df["Founded"].str[:4]
-    #print(df.head(10))
-    df.to_csv("sp500.csv", index=False)
+        df["Founded"] = df["Founded"].str[:4]
+        #print(df.head(10))
+        df.to_csv("sp500.csv", index=False)
+    except:
+        print("Error saving table")
+        exit(84)
 
 
 def main():
