3a8e84524efc08e5efc55a67c3ebdcd9f70830d1
Thomas <vaudescal.t@gmail.com>
2022-06-09 20:50:44 -0400

    refactoring of Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import re
 import json
 import csv
@@ -10,10 +12,7 @@ import streamlit as st
 #Le nom de la page
 st.markdown("Dashboard")
 
-
 df = pd.read_csv("sp500.csv")
-
-
 
 # On s'assure que pour chaque ticker le nom est unique
 tickers = df["Symbol"].unique()
@@ -21,69 +20,70 @@ tickers = df["Symbol"].unique()
 # On créer une side bar qui nous permettra de choisir le ticker dans l'application
 ticker = st.sidebar.selectbox("Choose a ticker", tickers)
 
-#Trouver le nom
-df2 = df.set_index("Symbol")
-Security_name = df2.loc[ticker].iat[0]
-
-
+security_name = df.loc[df["Symbol"] == ticker]["Security"].values[0]
+sector_name = df.loc[df["Symbol"] == ticker]["GICS Sector"].values[0]
+industry_name = df.loc[df["Symbol"] == ticker]["GICS Sub-Industry"].values[0]
 
 # On affiche ce qu'on à sélectionné dans la side bar
-ticker_col, Sec_col = st.columns(2)
-
-with ticker_col : st.write("You selected: ", ticker)
-
-with Sec_col :
-    st.write("Security name : ", Security_name )
+st.write("**Ticker**: ", ticker,
+         " || **Security:** ", security_name,
+         " || **Sector:** ", sector_name,
+         " || **Industry:** ", industry_name)
 
 
-#Company Information
+def scrapping_income_statement_from_yahoo_finance(ticker):
+    url_finance = 'https://ca.finance.yahoo.com/quote/{}/financials?p={}'
+    response = requests.Session().get(url_finance.format(ticker, ticker), headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.find_all("span")
+    text = [x.text for x in text]
 
-url_finance = 'https://ca.finance.yahoo.com/quote/{}/financials?p={}'
-
-
-
-response = requests.Session().get(url_finance.format(ticker, ticker),headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"})
-
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
-
-table = soup.find_all("span")
+    return text
 
 
-def has_numbers(x):
-    return any(char.isdigit() for char in x)
+text = scrapping_income_statement_from_yahoo_finance("AOS")
 
-Headers = []
-for i in table:
-    title=i.text
-    Headers.append(title)
+index_start = text.index("ttm")
+index_end = text.index("Net Income")
 
-index_start = Headers.index("ttm")
-index_end = Headers.index("Net Income")
+data = text[index_start: index_end+6]
 
-data = Headers[ index_start : index_end+6 ]
+regex_numbers = '-?\d+,?\d+,?\d+'
+data_numbers = [re.findall(regex_numbers, string) for string in data]
 
-#remove text
-data2 = [s for s in data[5:] if has_numbers(s)]
-rows= [s for s in data if has_numbers(s) == False]
+data_numbers_cleaned = [match for match in data_numbers[5:] if len(match) > 0]
+data_numbers_flat = sum(data_numbers_cleaned, [])
 
-rows.remove("Operating Expenses")
-rows.remove("ttm")
+regex_text = '\D+'
+data_texts = [re.findall(regex_text, string) for string in data]
 
+data_text_flat = []
 
-#Find the date
-date = data[0:5]
-df = pd.DataFrame(columns=date)
+for text in data_texts[1:]:
+    for word in text:
+        if len(word) > 1:
+            data_text_flat.append(word)
+
+regex_dates = '\d+-\d+-\d+'
+data_dates = [re.findall(regex_dates, string) for string in data]
+data_dates_cleaned = [date for date in data_dates if len(date) > 0]
+data_dates_flat = sum(data_dates_cleaned, [])
+
+data_dates_flat.insert(0, "ttm")
+
+print(data_dates_flat)
+
+df = pd.DataFrame(columns=data_dates_flat, index=data_text_flat)
 
 start = 0
 end = 5
-for x in range(int((len(data2))/5)):
-    to_append = data2[start:end]
-    df.loc[x] = to_append
-    start = start + 5
-    end = end + 5
 
-df_new = df.set_axis(rows)
+for i in range(len(df.index) - 1):
+    df.loc[df.index[i]] = data_numbers_flat[start:end]
+    start += 5
+    end += 5
 
-#On le met sur l'app
-st.dataframe(df_new)
+df.loc[df.index[-1]] = data_numbers_flat[-5:]
+
+st.write(df)
