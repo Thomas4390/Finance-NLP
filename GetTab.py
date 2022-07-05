d1b7b33dce073ad151931b1246f879c1191dc27e
Herosbrine <gregoiretheboss@gmail.com>
2022-07-05 16:33:13 +0200

    [FORMAT] | with Black

--- GetTab.py
+++ GetTab.py
@@ -3,13 +3,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
+
 def GetTitle(driver):
-    Title = driver.find_element(By.XPATH, '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Tt(u) Bgc($lv1BgColor)"]')
-    Title2 = driver.find_elements(By.XPATH, '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b)"]')
-    Title3 = driver.find_elements(By.XPATH, '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Bgc($lv1BgColor)"]')
-    MainTitle = Title.text + "," + Title2[0].text + "," + Title3[0].text + "," + Title2[1].text + "," + Title3[1].text
+    Title = driver.find_element(
+        By.XPATH,
+        '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Tt(u) Bgc($lv1BgColor)"]',
+    )
+    Title2 = driver.find_elements(
+        By.XPATH,
+        '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b)"]',
+    )
+    Title3 = driver.find_elements(
+        By.XPATH,
+        '//*[@class="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Bgc($lv1BgColor)"]',
+    )
+    MainTitle = (
+        Title.text
+        + ","
+        + Title2[0].text
+        + ","
+        + Title3[0].text
+        + ","
+        + Title2[1].text
+        + ","
+        + Title3[1].text
+    )
     with open("StatTab.csv", "w") as f:
         f.write("Breakdown" + "," + MainTitle + "\n")
+
 
 def GetTab():
     temp = 0
@@ -18,40 +39,44 @@ def GetTab():
     try:
         driver.get("https://ca.finance.yahoo.com/quote/ATVI/financials?p=ATVI")
         driver.set_page_load_timeout(30)
-        cookies = driver.find_elements(By.XPATH ,"//*[@class='btn secondary accept-all consent_reject_all_2']")
+        cookies = driver.find_elements(
+            By.XPATH, "//*[@class='btn secondary accept-all consent_reject_all_2']"
+        )
         sleep(0.5)
         cookies[0].click()
         GetTitle(driver)
-        MainTab = driver.find_elements(By.XPATH ,"//*[@class='D(tbrg)']")
+        MainTab = driver.find_elements(By.XPATH, "//*[@class='D(tbrg)']")
         ParseTab = MainTab[0].text
         ParseTab = ParseTab.replace(",", ".")
         ParseTab = ParseTab.split("\n")
         for element in ParseTab:
             with open("StatTab.csv", "a") as f:
-                #MANAGE FIRST COLUMN
-                if (temp == 0):
+                # MANAGE FIRST COLUMN
+                if temp == 0:
                     element = element + ","
                     f.write(element)
-                    #print(element, end="")
+                    # print(element, end="")
                     temp += 1
-                #MANAGE NUMBER 
-                elif (temp == 1 and element[0].isalpha() == False):
+                # MANAGE NUMBER
+                elif temp == 1 and element[0].isalpha() == False:
                     element = element.replace(" ", ",")
                     f.write(element + "\n")
-                    #print(element)
+                    # print(element)
                     temp = 0
-                #MANAGE EMPTY LINE
+                # MANAGE EMPTY LINE
                 else:
                     element = element + ","
                     f.write("\n" + element)
-                    #print("\n", element, end="")
+                    # print("\n", element, end="")
                     temp = 1
     except:
         print("Error")
         exit(84)
 
+
 def main():
     GetTab()
+
 
 if __name__ == "__main__":
     main()
