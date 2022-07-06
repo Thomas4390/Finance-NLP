6b3524aade6e5143368a8d1091f4bc9b480c2dd8
Herosbrine <gregoiretheboss@gmail.com>
2022-07-06 23:45:00 +0200

    [ADD] | error manage pietroski

--- pietroski.py
+++ pietroski.py
@@ -4,20 +4,24 @@ from bs4 import BeautifulSoup
 import requests
 
 def GetPietroskiScore(Company):
-    url = "https://www.gurufocus.com/term/fscore/NAS:" + Company + "/Piotroski-F-Score/"
-    response = requests.Session().get(url, headers={
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"})
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.find_all("font")
-    for element in text:
-        if ("(As of Today)" in element.text):
-            PietroskiFScore = element.text.replace("(As of Today)", "")
-            PietroskiFScore = PietroskiFScore.replace(" ", "")
-            PietroskiFScore = PietroskiFScore.replace(":", "")
-            print(Company, ":", PietroskiFScore)
+    try:
+        url = "https://www.gurufocus.com/term/fscore/NAS:" + Company + "/Piotroski-F-Score/"
+        response = requests.Session().get(url, headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.find_all("font")
+        for element in text:
+            if ("(As of Today)" in element.text):
+                PietroskiFScore = element.text.replace("(As of Today)", "")
+                PietroskiFScore = PietroskiFScore.replace(" ", "")
+                PietroskiFScore = PietroskiFScore.replace(":", "")
+                #print(Company, ":", PietroskiFScore)
+                return (PietroskiFScore)
+    except:
+        return("None")
 
 def main():
-    GetPietroskiScore("AAPL")
+    GetPietroskiScore("GOOGL")
 
 if __name__ == "__main__":
     main()
